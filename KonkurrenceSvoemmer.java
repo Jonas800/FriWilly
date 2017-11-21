05c051b60014bd995a5d51b311d782c986492150
Jonas800 <jonasolsenit@gmail.com>
2017-11-21 17:16:26 +0100

    more work sdone

--- KonkurrenceSvoemmer.java
+++ KonkurrenceSvoemmer.java
@@ -1,6 +1,26 @@
+import java.util.*;
+import java.io.*;
 public class KonkurrenceSvoemmer extends Medlem{
-   ArrayList<TraeningsResultat> trList;
-   ArrayList<StaevneResultat> stList;
+   private ArrayList<TraeningsResultat> trList;
+   private ArrayList<StaevneResultat> stList;
    private String traener;
   
+   public KonkurrenceSvoemmer(int id, String navn, String fdatoString, boolean aktivitetsform, String traener){
+      super(id, navn, fdatoString, aktivitetsform);
+      this.traener = traener;
+      this.stList = new ArrayList<StaevneResultat>();
+      this.trList = new ArrayList<TraeningsResultat>();
+      udfyldStList(stList);
+      udfyldTrList(trList);
+   }
+   
+   private void udfyldStList(ArrayList<StaevneResultat> list){
+      Scanner scanner = new Scanner(new File("staevneresultater.txt"));
+      
+   }
+   private void udfyldTrList(ArrayList<TraeningsResultat> list){
+      Scanner scanner = new Scanner(new File("traeningsresultater.txt"));
+
+   }
+   
 }
