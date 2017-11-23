093fa678bf49af72e22090deaa9c6fb6fc650e51
michjens <michael.diksi@hotmail.com>
2017-11-23 09:50:48 +0100

    menuer

--- Delfinen.java
+++ Delfinen.java
@@ -2,59 +2,106 @@ import java.util.*;
 
 public class Delfinen{
    public static void main(String [] args)throws Exception{
-      loadMenu();
+      visMenu();
    }
    
-   public static void loadMenu()throws Exception{
+   public static void visMenu()throws Exception{
       Scanner console = new Scanner(System.in);
       System.out.println("1: Formand\n2: Traener\n3: Kasserer\n4: Stop");
-      int menuChoice = console.nextInt();
-      while(menuChoice != 4){
-         switch(menuChoice){
+      int menuValg = console.nextInt();
+      while(menuValg != 4){
+         switch(menuValg){
             case 1:
                formand();
                break;
             case 2:
-               trainer();
+               traener();
                break;
             case 3:
-               cashier();
+               Kasserer();
                break;
             default:
-               loadMenu();
+               visMenu();
                break;
          }
       }
    }
    
-   
-   
    public static void formand()throws Exception{
-      Scanner input = new Scanner(System.in);
-      System.out.println("1: Register medlemmer\n2: Se medlemmer\n3: Opdater medlemmer\n4: Tilbage");
-         int formandChoice = input.nextInt();
-         if (formandChoice == 4){
-            loadMenu(); 
-         }else {
-            switch(formandChoice){
-               case 1: 
-                  registerMembers();
-                  break;
-               case 2:
-                  viewMembers();
-                  break;
-               case 3:
-                  updateMembers();
-                  break;
-               default:
-                  System.out.println("Forkert input. Proev igen");
-                  formand();
-                  break;
+      Scanner console = new Scanner(System.in);
+      System.out.println("1: Register medlemmer\n2: Se medlemmer\n3: Opdater medlemmer\n4: Tilbage til hovedmenu");
+         int formandValg = input.nextInt();
+         switch(formandValg){
+            case 1: 
+               registerMembers();
+               break;
+            case 2:
+               viewMembers();
+               break;
+            case 3:
+               updateMembers();
+               break;
+            case 4:
+               visMenu();
+               break;
+            default:
+               System.out.println("Ugyldigt input. Proev igen");
+               formand();
+               break;
                   
-            }
          }
    }
    
-   public static void 
-       
+   
+   public static void traener()throws Exception{
+      Scanner console = new Scanner(System.in);
+      System.out.println("1: Se konkurrencesvoemmere\n2: Se top 5\n3: Opdater resultater\n4: Tilbage til hovedmenu");
+      int traenerValg = console.nextInt();
+      switch(traenerValg){
+         case 1:
+            seKonkurrenceSvoemmere();
+            break;
+         case 2:
+            seTopFem();
+            break;
+         case 3:
+            opdaterResultater();
+            break;
+         case 4:
+            visMenu();
+            break;
+         default:
+            System.out.println("Ugyldigt input. Proev igen");
+            traener();
+            break;
+         
+      }
+      
+   }
+   
+   public static void kasserer(){
+      Scanner console = new Scanner(System.in);
+      System.out.println("1: Se medlemmer i restance\n2: Se medlemmers indbetaling i kontigent\n3: Håndter kontigent\n4: Tilbage til hovedmenu");
+      int kassererValg = console.nextInt();
+      switch(kassererValg){
+         case 1:
+            seRestance();
+            break;
+         case 2:
+            seIndbetaling();
+            break;
+         case 3:
+            haandterKontigent();
+            break;
+         case 4:
+            visMenu();
+            break;
+         default:
+            System.out.println("Ugyldigt input. Proev igen");
+            break;
+            
+      }
+   }    
+             
 }
+       
